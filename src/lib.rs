c42152783b186554e420c80de6c5431ca11d916e
Jesse A. Tov <jesse.tov@gmail.com>
2016-06-15 01:07:59 -0500

    Version: 0.0.0

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+//! Succinct Data Structures for Rust
+//!
+//! # Usage
+//!
+//! It’s [on crates.io](https://crates.io/crates/succinct), so you can add
+//!
+//! ```toml
+//! [dependencies]
+//! succinct = "0.0.0"
+//! ```
+//!
+//! to your `Cargo.toml` and
+//!
+//! ```rust
+//! extern crate succinct;
+//! ```
+//!
+//! to your crate root.
+
+#![warn(missing_docs)]
+
+#![cfg_attr(feature = "clippy", feature(plugin))]
+#![cfg_attr(feature = "clippy", plugin(clippy))]
+
