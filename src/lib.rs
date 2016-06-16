b25711057b55453b3f094ccdf0f6f4fe08d107d3
Jesse A. Tov <jesse.tov@gmail.com>
2016-06-15 21:06:15 -0500

    Exposed IntVecBuilder and BlockType

--- src/lib.rs
+++ src/lib.rs
@@ -25,12 +25,13 @@
 extern crate num;
 
 mod block_type;
+pub use block_type::*;
 
-pub mod int_vec;
-pub use int_vec::IntVec;
+mod int_vec;
+pub use int_vec::{IntVec, IntVecBuilder};
 
-pub mod bit_vector;
+mod bit_vector;
 pub use bit_vector::{Rank, Select};
 
-pub mod rank;
+mod rank;
 pub use rank::RankSupport;
