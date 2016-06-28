0f7eca0e67dc0853b34b8e3bfa2c1c16c2a5e03c
Jesse A. Tov <jesse.tov@gmail.com>
2016-06-28 15:54:46 -0500

    Renamed bit_vec::slice module to bit_vec::bit_slice.

--- src/bit_vec/mod.rs
+++ src/bit_vec/mod.rs
@@ -6,8 +6,8 @@ pub use self::traits::*;
 mod bit_vector;
 pub use self::bit_vector::*;
 
-mod slice;
-pub use self::slice::*;
+mod bit_slice;
+pub use self::bit_slice::*;
 
 mod prim;
 pub use self::prim::*;
